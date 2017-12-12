64060e8dca28fb8c6f91737f2a2377b317fbc7b6
alexmaggard <amaggard09@gmail.com>
2017-12-11 18:24:13 -0800

    fixed employeeDB

--- src/java/RTv2/database/EmployeeDB.java
+++ src/java/RTv2/database/EmployeeDB.java
@@ -19,18 +19,18 @@ public class EmployeeDB {
         PreparedStatement ps = null;
 
         String query
-                = "INSERT INTO cs_employees (FirstName, LastName, "
+                = "INSERT INTO cs_employees (EmployeeID, FirstName, LastName, "
                 + "AuthLevel, Status, PayRate, Password) "
-                + "VALUES (?, ?, ?, ?, ?, ?)";
+                + "VALUES (?, ?, ?, ?, ?, ?, ?)";
         try {
             ps = connection.prepareStatement(query);
-            //ps.setInt(1, employee.getEmployeeID());
-            ps.setString(1, employee.getFirstName());
-            ps.setString(2, employee.getLastName());
-            ps.setInt(3, employee.getAuthLevel());
-            ps.setBoolean(4, employee.getStatus());
-            ps.setDouble(5, employee.getPayRate());
-            ps.setString(6, "password");//set password default to password
+            ps.setInt(1, employee.getEmployeeID());
+            ps.setString(2, employee.getFirstName());
+            ps.setString(3, employee.getLastName());
+            ps.setInt(4, employee.getAuthLevel());
+            ps.setBoolean(5, false);
+            ps.setDouble(6, employee.getPayRate());
+            ps.setString(7, "password");//set password default to password
 
             return ps.executeUpdate();
         } catch (SQLException e) {
