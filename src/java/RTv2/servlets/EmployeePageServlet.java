6531b865ba649a62d894e765449a1d070b2db14c
jcAlcala <jc.alcala.87@gmail.com>
2017-12-17 19:12:13 -0800

    showMyHours working ang name stays

--- src/java/RTv2/servlets/EmployeePageServlet.java
+++ src/java/RTv2/servlets/EmployeePageServlet.java
@@ -5,7 +5,9 @@
  */
 package RTv2.servlets;
 
+import RTv2.database.EmployeeDB;
 import RTv2.database.TimeClockDB;
+import RTv2.objects.Employee;
 import RTv2.objects.TimeClock;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -24,12 +26,9 @@ import javax.swing.JOptionPane;
  * @author Admin
  */
 
-<<<<<<< HEAD
 @WebServlet(name = "EmployeePageServlet", urlPatterns = {"/employeePageServlet"})
-=======
-@WebServlet(name = "employeePageServlet", urlPatterns = {"/employeePageServlet"})
+//@WebServlet(name = "employeePageServlet", urlPatterns = {"/employeePageServlet"})
 
->>>>>>> origin/johnsBranch
 public class EmployeePageServlet extends HttpServlet {
 
    @Override
@@ -94,9 +93,15 @@ public class EmployeePageServlet extends HttpServlet {
         
         else if (action.equals("showMyHours")){
             int employeeID = Integer.parseInt(request.getParameter("employeeID"));
+            
             JOptionPane.showMessageDialog(null, employeeID);
-            TimeClock timeClock = TimeClockDB.selectTimeClock(employeeID);
-            session.setAttribute("timeClock",timeClock);
+            
+            ArrayList<TimeClock> timeClock = TimeClockDB.selectTimeClock(employeeID);
+            
+            Employee employee = EmployeeDB.selectEmployee(employeeID);
+            
+            request.setAttribute("timeClocks",timeClock);      
+            request.setAttribute("employee",employee);
             //TODO: add if statment to check authLevel based on
             //authLevel choose either manager or employee.jsp...
             url = "/employeePage.jsp";
