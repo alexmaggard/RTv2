24e78da9f625c2630ec66ce5139e72ee91af371c
Alexander Maggard <amaggard09@gmail.com>
2017-12-18 16:51:06 -0800

    removed unused imports

--- src/java/RTv2/database/TimeClockDB.java
+++ src/java/RTv2/database/TimeClockDB.java
@@ -14,8 +14,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Date;
 
